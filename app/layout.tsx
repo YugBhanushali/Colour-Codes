9a95c537661766d17c0e68f20999702eee5b373f
YugBhanushali <yughbhanushali241103@gmail.com>
2023-08-11 23:00:48 +0530

    updated meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,26 +14,6 @@ const robotoMono = Roboto_Mono({
 export const metadata = {
   title: 'Colour Codes',
   description: `Colour Codes is a comprehensive color converter app that simplifies the process of converting colors between various formats. Whether you're working with RGB, RGBA, HEX, HSL, or HSLA color codes, ColorCodes provides a seamless and intuitive solution to convert any color to all color formats.`,
-  metadata:[
-    { name: 'viewport', content: 'width=device-width, initial-scale=1'},
-    { name: 'description', content: `Colour Codes is a comprehensive color converter app that simplifies the process of converting colors between various formats. Whether you're working with RGB, RGBA, HEX, HSL, or HSLA color codes, ColorCodes provides a seamless and intuitive solution to convert any color to all color formats.`},
-    {name:'keywords' , content: "colour codes, color codes, color converter, colour converter, color picker, colour picker, color, colour, rgb, rgba, hex, hsl, hsla, rgb to hex, rgba to hex, rgb to hsl, rgba to hsl, rgb to hsla, rgba to hsla, hex to rgb, hex to rgba, hex to hsl, hex to hsla, hsl to rgb, hsl to rgba, hsl to hex, hsl to hsla, hsla to rgb, hsla to rgba, hsla to hex, hsla to hsl, color to hex, color to rgb, color to rgba, color to hsl, color to hsla, colour to hex, colour to rgb, colour to rgba, colour to hsl, colour to hsla"},
-
-    { property: 'og:site_name', content: 'Colour Codes' },
-    { property: 'og:title', content: 'Colour Codes' },
-    { property: 'og:description', content: `Colour Codes is a comprehensive color converter app that simplifies the process of converting colors between various formats. Whether you're working with RGB, RGBA, HEX, HSL, or HSLA color codes, ColorCodes provides a seamless and intuitive solution to convert any color to all color formats.`},
-    { property: 'og:url', content: WEBSITE_URL },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:image', content: `${WEBSITE_URL}/icon.png` },
-    { property: 'og:image:alt', content: 'Colour Codes' },
-
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Colour Codes' },
-    { name: 'twitter:description', content: `Colour Codes is a comprehensive color converter app that simplifies the process of converting colors between various formats. Whether you're working with RGB, RGBA, HEX, HSL, or HSLA color codes, ColorCodes provides a seamless and intuitive solution to convert any color to all color formats.`},
-    { name: 'twitter:image', content: `${WEBSITE_URL}/icon.png` },
-    { name: 'twitter:image:alt', content: 'Colour Codes' },
-
-  ]
 }
 
 export default function RootLayout({
@@ -44,6 +24,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>{metadata.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content={metadata.description}/>
+        
+        <meta property="og:site_name" content="Colour Codes" />
+        <meta property="og:title" content="Colour Codes" />
+        <meta property="og:description" content={metadata.description}/>
+        <meta property="og:url" content={WEBSITE_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={`${WEBSITE_URL}/icon.png`} />
+        <meta property="og:image:alt" content="Colour Codes" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Colour Codes" />
+        <meta name="twitter:description" content={metadata.description}/>
+        <meta name="twitter:image" content={`${WEBSITE_URL}/icon.png`} />
+        <meta name="twitter:image:alt" content="Colour Codes" />
+        
         <link rel="icon" href="/icon.png" type='image/png' />
       </head>
       <body className={robotoMono.className}>
